912018f8bde1b82a6b4dbdbf837487ce1940aa37
cosmic-blunder <sahil18bc07@gmail.com>
2022-09-18 00:37:21 +0530

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cosmic-blunder/monklang
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import ("fmt"
+       // "github.com/oscuro1111/golang-libs/semaphore"
+      // "github.com/google/go-cmp/cmp"
+         "github.com/cosmic-blunder/monklang/utils"
+)
+func main(){
+
+     fmt.Println("Hello monk lang");
+
+     fmt.Println(utils.Util());
+}
