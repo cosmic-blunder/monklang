7ae31d43e773891d436face514ddfc0cf6ca9db0
cosmic-blunder <sahil18bc07@gmail.com>
2022-09-20 23:24:37 +0530

    base test setup

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -32,17 +32,16 @@ func TestNextToken(t *testing.T){
 
       l:=New(input)
 
-
       for i, tt := range tests {
            tok:=l.NextToken()
 
 	   if tok.Type!=tt.expectedType{
 
-              t.Fatalf("test[%d] -  tokentype wrong.expected=%q,got=%q",i,tt.expected,token.Type)
+              t.Fatalf("test[%d] -  tokentype wrong.expected=%q,got=%q",i,tt.expectedType,tok.Type)
 	   }
 
 	   if tok.Literal != tt.expectedLiteral{
-               t.Fatalf("test[%d] - literal wrong. expected=%q , got=%q",i,ii.expectedLiteral,tok.Lietral);
+               t.Fatalf("test[%d] - literal wrong. expected=%q , got=%q",i,tt.expectedLiteral,tok.Literal);
 	   }
       }
 
